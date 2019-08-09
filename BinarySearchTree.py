38822ad0421be9933fb54456e735e4fa753a42d1
zxzrerster <zxzerster@gmail.com>
2019-08-09 10:21:33 +0800

    BinarySearchTree: Insertion (iterative / recursive)

---
+++ BinarySearchTree.py
@@ -0,0 +1,65 @@
+class BinarySearchTree:
+    class Node:
+        def __init__(self, val):
+            self._left = None
+            self._right = None
+            self._val = val
+
+    def __init__(self):
+        self._root = None
+
+    def insert(self, node):
+        if node is None: return
+
+        if self._root is None:
+            self._root = node
+            return
+
+        BinarySearchTree._recursive_insert(self.root, node)
+
+    @staticmethod
+    def _recursive_insert(root, node):
+        if node._val < root._val:
+            if root._left is not None:
+                BinarySearchTree._recursive_insert(root._left, node)
+            else:
+                root._left = node
+        else:
+            if root._right is not None:
+                BinarySearchTree._recursive_insert(root._right, node)
+            else:
+                root._right = node
+
+    @staticmethod
+    def _iteraitve_insert(root, node):
+        n = root
+        p = n
+
+        while n is not None:
+            p = n
+            if node._val < n._val:
+                n = n._left
+            else:
+                n = n._right
+
+        # Find insertion place, do insertion
+        if node._val < p._val:
+            p._left = node
+        else:
+            p._right = node
+
+
+if __name__ == "__main__":
+    print("Test BinarySearchTree")
+
+    print("#" * 10 + " Insert / Search / Delete " + "#" * 10)
+    t = BinarySearchTree()
+
+    n = BinarySearchTree.Node(8)
+    t.insert(n)
+
+    n = BinarySearchTree.Node(4)
+    t.insert(n)
+
+    n = BinarySearchTree.Node(3)
+    t.insert(n)
