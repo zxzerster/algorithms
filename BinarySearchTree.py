6e7845f4235fe48991460eb592f775a6031b7dfa
zxzrerster <zxzerster@gmail.com>
2019-08-12 14:52:02 +0800

    BinearySearchTree: Successort - consider 3 cases

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -40,6 +40,35 @@ class BinarySearchTree:
 
         return p
 
+    def successor(self, node):
+        if node is None: return None
+
+        #  case 1: node is max itself, there will be no successor for max node
+        r = self.max(self._root)
+        if r == node: return None
+
+        # case 2: node is not max node, and have right sub-tree, successor will be
+        # min of right sub-tree
+        if node._right is not None:
+            return self.min(node._right)
+
+        # case 3: node is not max node, and doesn't have right sub-tree, successor will be
+        # nearest parent whoes first sub-left child is node
+
+        # because we don't have parent pointer in each node, we have to search the nearest "left parent"
+        # from top to bottom
+        # left parent -> means node is the left child of parent
+        n = self._root
+        while n is not node:
+            p = n
+            if node._val < n._val:
+                lp = n
+                n = n._left
+            else:
+                n = n._right
+
+        return lp
+
     @staticmethod
     def _recursive_insert(root, node):
         if node._val < root._val:
@@ -80,31 +109,73 @@ if __name__ == "__main__":
 
     n = BinarySearchTree.Node(8)
     t.insert(n)
+    n8 = n
 
     n = BinarySearchTree.Node(4)
     t.insert(n)
+    n4 = n
 
     n = BinarySearchTree.Node(3)
     t.insert(n)
+    n3 = n
 
     n = BinarySearchTree.Node(15)
     t.insert(n)
+    n15 = n
 
     n = BinarySearchTree.Node(11)
     t.insert(n)
+    n11 = n
 
     n = BinarySearchTree.Node(19)
     t.insert(n)
+    n19 = n
+
+    n = BinarySearchTree.Node(13)
+    t.insert(n)
+    n13 = n
+
+    n = BinarySearchTree.Node(17)
+    t.insert(n)
+    n17 = n
+
+    n = BinarySearchTree.Node(23)
+    t.insert(n)
+    n23 = n
 
     print("#" * 10 + " Min / Max " + "#" * 10)
     m1 = t.min(t.root())
     m2 = t.max(t.root())
 
     assert(m1._val == 3)
-    assert(m2._val == 19)
+    assert(m2._val == 23)
 
     m1 = None
     m2 = None
 
-    n = None
+    print("#" * 10 + " Successor " + "#" * 10)
+    # case 1: max node donesn't have a successor
+    s = t.successor(n23)
+    assert(s is None)
+
+    # case 2: node has right sub-tree
+    s = t.successor(n15)
+    assert(s._val == 17)
+
+    # case 3: node only has left sub-tree
+    s = t.successor(n4)
+    assert(s._val == 8)
+
+    del m1
+    del m2
+    del n
+    del n8
+    del n4
+    del n3
+    del n15
+    del n11
+    del n19
+    del n13
+    del n17
+    del n23
     print("End") #break
